4b2b450db98da6b37b1f01dd9b23bf488d1f1b2c
Alexandr Staloverov <staloverov@rosebud.ru>
2025-08-10 21:16:07 +0300

    fix: Исправлена ошибка ресайза на мобильных устройствах

--- src/widgets/ui/header/Header.svelte
+++ src/widgets/ui/header/Header.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Logo from '$shared/assets/svg/icon.svg?component';
 	import { Button } from '$shared/ui';
 	import Figure2 from './fig2.svg?component';
 	import logo from './assets/logo.png';
@@ -11,23 +10,25 @@
 	};
 </script>
 
-<div class="main-container">
-	<img src={logo} alt={'logo'} class="logo centered" />
-	<div class="text-box">
-		<div class="text first-row text--Dark_Blue">Создавайте</div>
-		<div class="text second-row text--Dark_Blue">IT-продукты</div>
-		<div class="text third-row text--Primary_Blue">с Адмиралом</div>
+<div class="header-container">
+	<div class="elipse1"></div>
+	<div class="figure2">
+		<Figure2 />
 	</div>
-	<div class="centered">
-		<Button onclick={handleButtonClick}>Сторибук</Button>
-	</div>
-	<img src={leftFigure} alt={'leftFigure'} class="left-figure element-to-animate" />
-	<img src={rightFigure} alt={'rightFigure'} class="right-figure element-to-animate" />
-</div>
 
-<div class="elipse1"></div>
-<div class="figure2">
-	<Figure2 />
+	<div class="main-container">
+		<img src={logo} alt={'logo'} class="logo centered" />
+		<div class="text-box">
+			<div class="text first-row text--Dark_Blue">Создавайте</div>
+			<div class="text second-row text--Dark_Blue">IT-продукты</div>
+			<div class="text third-row text--Primary_Blue">с Адмиралом</div>
+		</div>
+		<div class="centered">
+			<Button onclick={handleButtonClick}>Сторибук</Button>
+		</div>
+		<img src={leftFigure} alt={'leftFigure'} class="left-figure element-to-animate" />
+		<img src={rightFigure} alt={'rightFigure'} class="right-figure element-to-animate" />
+	</div>
 </div>
 
 <style lang="scss">
