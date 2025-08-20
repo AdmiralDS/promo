703f8a855bc0756b927b53f79a1d7b9295639725
Larisa Markova <lovemirror@rambler.ru>
2025-08-20 16:06:35 +0300

    Add cell component

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,4 +1,6 @@
 export { default as Button, type ButtonProps } from './atoms/button/Button.svelte';
 export { default as ComponentContainer } from './atoms/componentContainer/ComponentContainer.svelte';
 export { default as CardContainer, type CardContainerProps } from './atoms/cardContainer/CardContainer.svelte';
+export { default as CellContainer, type CellContainerProps } from './atoms/cellContainer/CellContainer.svelte';
 export { default as Card } from './molecules/card/Card.svelte';
+export { default as Cell } from './molecules/cell/Cell.svelte';
