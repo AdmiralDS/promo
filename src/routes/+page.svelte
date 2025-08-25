324b5448d649834d7e199753636b75ddc7ba6d36
Kutepova Ekaterina <eovetelina@gmail.com>
2025-08-25 17:45:12 +0300

    add comments

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import Header from '$widgets/ui/header/Header.svelte';
-	import Products from '$widgets/ui/products/Products.svelte';
+	// import Products from '$widgets/ui/products/Products.svelte';
 </script>
 
 <Header></Header>
-<Products></Products>
+<!--Раздел Products для тестирования модалки-->
+<!-- <Products></Products> -->
