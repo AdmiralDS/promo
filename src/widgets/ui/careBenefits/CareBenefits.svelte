e273c0b16865071603863aa28aae9d75857fb079
Larisa Markova <lovemirror@rambler.ru>
2025-09-08 18:07:45 +0300

    Add cards box

--- src/widgets/ui/careBenefits/CareBenefits.svelte
+++ src/widgets/ui/careBenefits/CareBenefits.svelte
@@ -1,4 +1,23 @@
 <script lang="ts">
+	import { Card } from '$shared/ui';
+
+	const cards = $state([
+		{
+			id: 1,
+			title: 'Ускоряем создание продуктов',
+			text: 'Помогаем бизнесу быстро тестировать гипотезы и выводить решения на рынок'
+		},
+		{
+			id: 2,
+			title: 'Формируем единый визуальный стиль',
+			text: 'Строим экосистему визуальных решений, которая повышает доверие пользователей'
+		},
+		{
+			id: 3,
+			title: 'Создаем комфортное пространство',
+			text: 'Помогаем командам говорить на одном языке и улучшать процесс доставки решений'
+		}
+	]);
 </script>
 
 <div class="carebenefits-container">
@@ -7,7 +26,11 @@
 			<div class="text first-row text--Dark_Blue">Заботимся не только о качестве,</div>
 			<div class="text second-row text--Text_Blue">но и о вашем времени</div>
 		</div>
-		<div class="cards-box"></div>
+		<div class="cards-box">
+			{#each cards as card (card.id)}
+				<Card class="cards-box-item" title={card.title} text={card.text} appearance="primary"></Card>
+			{/each}
+		</div>
 	</div>
 </div>
 
