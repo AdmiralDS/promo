a68555e3923351574c495c56fe87cf6a413af6e3
Larisa Markova <lovemirror@rambler.ru>
2025-09-08 16:55:56 +0300

    Add section CareBenefits

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import Header from '$widgets/ui/header/Header.svelte';
+	import CareBenefits from '$widgets/ui/careBenefits/CareBenefits.svelte';
 	// import Products from '$widgets/ui/products/Products.svelte';
 </script>
 
 <Header></Header>
+<CareBenefits></CareBenefits>
 <!--Раздел Products для тестирования модалки-->
 <!-- <Products></Products> -->
---
+++ src/widgets/ui/careBenefits/CareBenefits.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<div class="carebenefits-container">
+	<div class="main-container">
+		<div class="text-box">
+			<div class="text first-row text--Dark_Blue">Заботимся не только о качестве,</div>
+			<div class="text second-row text--Text_Blue">но и о вашем времени</div>
+		</div>
+		<div class="cards-box"></div>
+	</div>
+</div>
+
+<style lang="scss">
+	@use 'styles';
+</style>
