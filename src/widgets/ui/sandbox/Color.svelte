249afa90e8e9837d0a9c9fb276b435acf32c4c78
Larisa Markova <lovemirror@rambler.ru>
2025-12-22 15:58:15 +0300

    Add icons

---
+++ src/widgets/ui/sandbox/Color.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	export let width: string = '32';
+	export let height: string = '32';
+	export let viewBox: string = '0 0 32 32';
+	export let fill: string = 'none';
+</script>
+
+<svg xmlns="http://www.w3.org/2000/svg" {width} {height} {viewBox} {fill}>
+	<g clip-path="url(#clip0_2564_1339)">
+		<circle cx="16" cy="16" r="16" fill="#006FFF" />
+	</g>
+	<defs>
+		<clipPath id="clip0_2564_1339">
+			<rect width="32" height="32" fill="white" />
+		</clipPath>
+	</defs>
+</svg>
