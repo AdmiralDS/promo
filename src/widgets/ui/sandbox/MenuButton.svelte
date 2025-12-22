02c7fc9d1b9e9c7b3016bb0534727e71b63d5e92
Larisa Markova <lovemirror@rambler.ru>
2025-12-22 15:58:29 +0300

    Add MenuButton

---
+++ src/widgets/ui/sandbox/MenuButton.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Chevron from './Chevron.svelte';
+	let isOpen = false;
+
+	const toggleMenu = () => {
+		isOpen = !isOpen;
+	};
+</script>
+
+<div class="menu-button">
+	<button type="button" aria-expanded={isOpen} on:click={toggleMenu}>
+		<div class="icon-wrapper">
+			<slot name="icon"></slot>
+		</div>
+		<div class="chevron-wrapper" class:open={isOpen}>
+			<Chevron />
+		</div>
+	</button>
+
+	{#if isOpen}
+		<div class="dropdown-wrapper">
+			<slot name="dropdown" />
+		</div>
+	{/if}
+</div>
+
+<style lang="scss">
+	@use 'menuButton';
+</style>
