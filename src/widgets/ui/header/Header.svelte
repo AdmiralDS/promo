d1937978bdafe148221c0b4913ec26fd9cad52ff
Larisa Markova <lovemirror@rambler.ru>
2026-01-26 12:45:18 +0300

    First try

--- src/widgets/ui/header/Header.svelte
+++ src/widgets/ui/header/Header.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { Button } from '$shared/ui';
+	import { DotLottieSvelte } from '@lottiefiles/dotlottie-svelte';
 	import Figure2 from './fig2.svg?component';
 	import logo from './assets/logo.png';
 	import leftFigure from './assets/left.png';
@@ -11,23 +12,8 @@
 </script>
 
 <div class="header-container">
-	<div class="elipse1"></div>
-	<div class="figure2">
-		<Figure2 />
-	</div>
-
-	<div class="main-container">
-		<img src={logo} alt={'logo'} class="logo centered" />
-		<div class="text-box">
-			<div class="text first-row text--Dark_Blue">Создавайте</div>
-			<div class="text second-row text--Dark_Blue">IT-продукты</div>
-			<div class="text third-row text--Text_Blue">с Адмиралом</div>
-		</div>
-		<div class="centered">
-			<Button onclick={handleButtonClick}>Сторибук</Button>
-		</div>
-		<img src={leftFigure} alt={'leftFigure'} class="left-figure element-to-animate" />
-		<img src={rightFigure} alt={'rightFigure'} class="right-figure element-to-animate" />
+	<div class="lottie-hero" aria-hidden="true">
+		<DotLottieSvelte src="/animation1.json" autoplay loop />
 	</div>
 </div>
 
