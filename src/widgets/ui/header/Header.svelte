ac4eddc184ce3b8e820b18578925bc8e02747d48
Larisa Markova <lovemirror@rambler.ru>
2026-01-26 12:45:18 +0300

    First try

--- src/widgets/ui/header/Header.svelte
+++ src/widgets/ui/header/Header.svelte
@@ -1,13 +1,22 @@
 <script lang="ts">
+  import { DotLottieSvelte } from '@lottiefiles/dotlottie-svelte';
   import { Button } from '$shared/ui';
-  // import Figure2 from './fig2.svg?component';
-  import LeftCloud from './leftCloud.svg?component';
-  import RightCloud from './rightCloud.svg?component';
   import logo from './assets/logo.png';
   import heroDesktop from './assets/Hero_1920-1024.png';
   import heroTablet from './assets/Hero_1023-700.png';
   import heroMobile from './assets/Hero_699-360.png';
-  import { MOBILE_QUERY, TABLET_QUERY } from '$shared/ui/useMediaQuery';
+  import animation699 from './assets/animation/699.json?url';
+  import animation1023 from './assets/animation/1023.json?url';
+  import animation1239 from './assets/animation/1239.json?url';
+  import animation1920 from './assets/animation/1920.json?url';
+  import { useMediaQuery, MOBILE_QUERY, TABLET_QUERY, DESKTOP_S_QUERY } from '$shared/ui/useMediaQuery';
+
+  const animationSources = [animation699, animation1023, animation1239, animation1920];
+  const isMobile = useMediaQuery(MOBILE_QUERY);
+  const isTabletOrSmaller = useMediaQuery(TABLET_QUERY);
+  const isDesktopSOrSmaller = useMediaQuery(DESKTOP_S_QUERY);
+  const breakpointIndex = $derived($isMobile ? 0 : $isTabletOrSmaller ? 1 : $isDesktopSOrSmaller ? 2 : 3);
+  const animationSrc = $derived(animationSources[breakpointIndex]);
 
   const handleButtonClick = () => {
     window.open('https://admiralds.github.io/react-ui/', '_blank');
@@ -15,14 +24,6 @@
 </script>
 
 <div class="header-container">
-  <!-- <div class="elipse1"></div> -->
-  <div class="cloud">
-    <LeftCloud />
-  </div>
-  <div class="cloud right">
-    <RightCloud />
-  </div>
-
   <div class="main-container">
     <img src={logo} alt={'logo'} class="logo centered" />
     <div class="text-box">
@@ -38,6 +39,9 @@
       <source srcset={heroTablet} media={TABLET_QUERY} />
       <img src={heroDesktop} alt="Hero" loading="lazy" />
     </picture>
+  </div>
+  <div class="lottie-hero" aria-hidden="true">
+    <DotLottieSvelte src={animationSrc} autoplay loop />
   </div>
 </div>
 
