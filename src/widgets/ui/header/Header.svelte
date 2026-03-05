e22085b33139a7ed671c8b66bf19cd45cfc74990
VeronaPl <vp31102003@gmail.com>
2026-03-05 03:47:45 +0300

    fix(Header): change img, fix clouds, sizes, paddings and overflow styles

--- src/widgets/ui/header/Header.svelte
+++ src/widgets/ui/header/Header.svelte
@@ -4,8 +4,10 @@
 	import LeftCloud from './leftCloud.svg?component';
 	import RightCloud from './rightCloud.svg?component';
 	import logo from './assets/logo.png';
-	import leftFigure from './assets/left.png';
-	import rightFigure from './assets/right.png';
+	import heroDesktop from './assets/Hero_1920-1024.png';
+	import heroTablet from './assets/Hero_1023-700.png';
+	import heroMobile from './assets/Hero_699-360.png';
+	import { MOBILE_QUERY, TABLET_QUERY } from '$shared/ui/useMediaQuery';
 
 	const handleButtonClick = () => {
 		window.open('https://admiralds.github.io/react-ui/', '_blank');
@@ -14,7 +16,7 @@
 
 <div class="header-container">
 	<!-- <div class="elipse1"></div> -->
-	<div class="cloud">
+	<div class="cloud left">
 		<LeftCloud />
 	</div>
 	<div class="cloud right">
@@ -26,13 +28,16 @@
 		<div class="text-box">
 			<div class="text first-row text--Dark_Blue">Создавайте</div>
 			<div class="text second-row text--Dark_Blue">IT-продукты</div>
-			<div class="text third-row text--Text_Blue">с Адмиралом</div>
+			<div class="text text--Text_Blue">с Адмиралом</div>
 		</div>
 		<div class="centered">
 			<Button onclick={handleButtonClick}>Сторибук</Button>
 		</div>
-		<img src={leftFigure} alt={'leftFigure'} class="left-figure element-to-animate" />
-		<img src={rightFigure} alt={'rightFigure'} class="right-figure element-to-animate" />
+		<picture class="hero centered">
+			<source srcset={heroMobile} media={MOBILE_QUERY} />
+			<source srcset={heroTablet} media={TABLET_QUERY} />
+			<img src={heroDesktop} alt="Hero" loading="lazy" />
+		</picture>
 	</div>
 </div>
 
