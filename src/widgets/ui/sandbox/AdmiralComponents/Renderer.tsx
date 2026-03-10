bdbb3ad23f9eb2fcc554d83966c38b6c16436ee2
Larisa Markova <lovemirror@rambler.ru>
2026-03-10 09:54:44 +0300

    Remove comment

--- src/widgets/ui/sandbox/AdmiralComponents/Renderer.tsx
+++ src/widgets/ui/sandbox/AdmiralComponents/Renderer.tsx
@@ -1,18 +1,17 @@
-// Renderer.ts
 import { createRoot } from 'react-dom/client';
 import { WrappedReactComponent, type Props as Options } from './WrappedReactComponent';
 
 export const renderer = (container: HTMLDivElement, options: Options) => {
 	const root = createRoot(container);
-	
+
 	// Функция для рендера с опциями
 	const render = (newOptions: Options) => {
 		root.render(<WrappedReactComponent {...newOptions} />);
 	};
-	
+
 	// Начальный рендер
 	render(options);
-	
+
 	// Возвращаем объект с методом updateProps
 	return {
 		updateProps: (newOptions: Options) => {
