ef00c957c1bf4352d4026b11b8ed6b17c95f30aa
Larisa Markova <lovemirror@rambler.ru>
2026-04-06 16:46:45 +0300

    Fix repeated data

--- src/widgets/ui/careBenefits/CareBenefits.svelte
+++ src/widgets/ui/careBenefits/CareBenefits.svelte
@@ -27,49 +27,29 @@
 
   const breakpointIndex = $derived($isMobile ? 0 : $isTabletOrSmaller ? 1 : $isDesktopSOrSmaller ? 2 : 3);
 
-  const cards = $derived(
-    $isDesktopSOrSmaller
-      ? [
-          {
-            id: 2,
-            title: 'Формируем единый визуальный стиль',
-            text: 'Строим экосистему визуальных решений, которая повышает доверие пользователей',
-            img: styleImages[breakpointIndex],
-          },
-          {
-            id: 1,
-            title: 'Ускоряем создание продуктов',
-            text: 'Помогаем бизнесу быстро тестировать гипотезы и выводить решения на рынок',
-            img: speedImages[breakpointIndex],
-          },
-          {
-            id: 3,
-            title: 'Создаем комфортное пространство',
-            text: 'Помогаем командам говорить на одном языке и улучшать процесс доставки решений',
-            img: spaceImages[breakpointIndex],
-          },
-        ]
-      : [
-          {
-            id: 1,
-            title: 'Ускоряем создание продуктов',
-            text: 'Помогаем бизнесу быстро тестировать гипотезы и выводить решения на рынок',
-            img: speedImages[breakpointIndex],
-          },
-          {
-            id: 2,
-            title: 'Формируем единый визуальный стиль',
-            text: 'Строим экосистему визуальных решений, которая повышает доверие пользователей',
-            img: styleImages[breakpointIndex],
-          },
-          {
-            id: 3,
-            title: 'Создаем комфортное пространство',
-            text: 'Помогаем командам говорить на одном языке и улучшать процесс доставки решений',
-            img: spaceImages[breakpointIndex],
-          },
-        ],
-  );
+  const cards = $derived([
+    {
+      id: 1,
+      title: 'Ускоряем создание продуктов',
+      text: 'Помогаем бизнесу быстро тестировать гипотезы и выводить решения на рынок',
+      img: speedImages[breakpointIndex],
+    },
+    {
+      id: 2,
+      title: 'Формируем единый визуальный стиль',
+      text: 'Строим экосистему визуальных решений, которая повышает доверие пользователей',
+      img: styleImages[breakpointIndex],
+    },
+    {
+      id: 3,
+      title: 'Создаем комфортное пространство',
+      text: 'Помогаем командам говорить на одном языке и улучшать процесс доставки решений',
+      img: spaceImages[breakpointIndex],
+    },
+  ]);
+
+  const cardsOrder = $derived($isDesktopSOrSmaller ? [1, 0, 2] : [0, 1, 2]);
+  const orderedCards = $derived(cardsOrder.map((index) => cards[index]));
 </script>
 
 <div class="carebenefits-container">
@@ -81,7 +61,7 @@
       <div class="text second-row text--Text_Blue">но и о вашем времени</div>
     </div>
     <div class="cards-box">
-      {#each cards as card}
+      {#each orderedCards as card}
         <Card class="cards-box-item" title={card.title} text={card.text} appearance="primary">
           <img class="cards-box-img" src={card.img} alt="CellImg" />
         </Card>
