ef174876f381b53bd86b867c82c70f5d826e2d14
Larisa Markova <lovemirror@rambler.ru>
2026-04-10 17:46:05 +0300

    Update header animation

--- src/widgets/ui/mainWrapper/animations/HeaderAnimation.svelte
+++ src/widgets/ui/mainWrapper/animations/HeaderAnimation.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import ResponsiveAnimationVideo from './ResponsiveAnimationVideo.svelte';
-  import animationDesktopVideoMp4 from './assets/block_1_header/1920.hevc.mp4';
-  import animationDesktopSVideoMp4 from './assets/block_1_header/1239.hevc.mp4';
-  import animationTabletVideoMp4 from './assets/block_1_header/1023.hevc.mp4';
-  import animationMobileVideoMp4 from './assets/block_1_header/699.hevc.mp4';
+  import animationDesktopVideoMp4 from './assets/block_1_header/1920.mp4';
+  import animationDesktopSVideoMp4 from './assets/block_1_header/1248.mp4';
+  import animationTabletVideoMp4 from './assets/block_1_header/1024.mp4';
+  import animationMobileVideoMp4 from './assets/block_1_header/704.mp4';
 
   let { top = 0 }: { top?: number } = $props();
 </script>
