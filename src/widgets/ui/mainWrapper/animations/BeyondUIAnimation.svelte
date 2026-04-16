1e708200d40f1f3ccca530a9c16579cbcf949b2a
Larisa Markova <lovemirror@rambler.ru>
2026-04-16 15:35:22 +0300

    Update rest animation

--- src/widgets/ui/mainWrapper/animations/BeyondUIAnimation.svelte
+++ src/widgets/ui/mainWrapper/animations/BeyondUIAnimation.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import ResponsiveAnimationVideo from './ResponsiveAnimationVideo.svelte';
-  import animationDesktopVideoMp4 from './assets/block_3_beyond-ui/1920.hevc.mp4';
-  import animationDesktopSVideoMp4 from './assets/block_3_beyond-ui/1239.hevc.mp4';
-  import animationTabletVideoMp4 from './assets/block_3_beyond-ui/1023.hevc.mp4';
-  import animationMobileVideoMp4 from './assets/block_3_beyond-ui/699.hevc.mp4';
+  import animationDesktopVideoMp4 from './assets/block_3_beyond-ui/1920.mp4';
+  import animationDesktopSVideoMp4 from './assets/block_3_beyond-ui/1248.mp4';
+  import animationTabletVideoMp4 from './assets/block_3_beyond-ui/1024.mp4';
+  import animationMobileVideoMp4 from './assets/block_3_beyond-ui/704.mp4';
 
   let { top = 0 }: { top?: number } = $props();
 </script>
@@ -11,7 +11,7 @@
 <ResponsiveAnimationVideo
   align="center"
   {top}
-  fixedHeights={{ desktop: 1614, desktopS: 1540, tablet: 1478, mobile: 1822 }}
+  fixedHeights={{ desktop: 1672, desktopS: 1600, tablet: 1576, mobile: 1888 }}
   desktop={animationDesktopVideoMp4}
   desktopS={animationDesktopSVideoMp4}
   tablet={animationTabletVideoMp4}
--- src/widgets/ui/mainWrapper/animations/CareBenefitsAnimation.svelte
+++ src/widgets/ui/mainWrapper/animations/CareBenefitsAnimation.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
   import ResponsiveAnimationVideo from './ResponsiveAnimationVideo.svelte';
-  import animationDesktopVideoMp4 from './assets/block_2_care-benefits/1920.hevc.mp4';
-  import animationDesktopSVideoMp4 from './assets/block_2_care-benefits/1239.hevc.mp4';
-  import animationTabletVideoMp4 from './assets/block_2_care-benefits/1023.hevc.mp4';
-  import animationMobileVideoMp4 from './assets/block_2_care-benefits/699.hevc.mp4';
+  import animationDesktopVideoMp4 from './assets/block_2_care-benefits/1920.mp4';
+  import animationDesktopSVideoMp4 from './assets/block_2_care-benefits/1248.mp4';
+  import animationTabletVideoMp4 from './assets/block_2_care-benefits/1024.mp4';
+  import animationMobileVideoMp4 from './assets/block_2_care-benefits/704.mp4';
 
   let { top = 0 }: { top?: number } = $props();
 </script>
@@ -11,7 +11,7 @@
 <ResponsiveAnimationVideo
   align="center"
   {top}
-  fixedHeights={{ desktop: 1104, desktopS: 1008, tablet: 960, mobile: 1280 }}
+  fixedHeights={{ desktop: 1144, desktopS: 1056, tablet: 1056, mobile: 1336 }}
   desktop={animationDesktopVideoMp4}
   desktopS={animationDesktopSVideoMp4}
   tablet={animationTabletVideoMp4}
