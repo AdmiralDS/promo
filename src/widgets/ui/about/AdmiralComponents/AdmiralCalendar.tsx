3df94e9a28f1c1813e798ac299a30bb4f15622a0
Larisa Markova <lovemirror@rambler.ru>
2026-05-07 14:44:51 +0300

    Fix onChange

--- src/widgets/ui/about/AdmiralComponents/AdmiralCalendar.tsx
+++ src/widgets/ui/about/AdmiralComponents/AdmiralCalendar.tsx
@@ -5,15 +5,12 @@ import { Calendar } from '@admiral-ds/react-ui';
 export const AdmiralCalendar = () => {
   const [startDate, setStartDate] = React.useState<Date | null>(null);
   const [endDate, setEndDate] = React.useState<Date | null>(null);
-  return (
-    <Calendar
-      range
-      startDate={startDate}
-      endDate={endDate}
-      onChange={(value: any) => {
-        setStartDate(value[0]);
-        setEndDate(value[1]);
-      }}
-    />
-  );
+  const handleChange = (value: Date | Array<Date | null> | null) => {
+    if (Array.isArray(value)) {
+      setStartDate(value[0] ?? null);
+      setEndDate(value[1] ?? null);
+    }
+  };
+
+  return <Calendar range startDate={startDate} endDate={endDate} onChange={handleChange} />;
 };
